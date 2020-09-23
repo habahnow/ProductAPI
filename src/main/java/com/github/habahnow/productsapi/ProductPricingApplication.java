e02434f59e71d84c857dc12f8deb7739f98a0b5d
habahnow <danielsoto03@gmail.om>
2020-09-22 19:24:28 -0700

    Project Setup

---
+++ src/main/java/com/github/habahnow/productsapi/ProductPricingApplication.java
@@ -0,0 +1,13 @@
+package com.github.habahnow.productsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPricingApplication.class, args);
+	}
+
+}
